bc35d9bda6a6e7c838adcb05d3a662b4d14b8416
kalev <artkalev@gmail.com>
2025-01-08 20:53:49 +0200

    progress on gallery

--- assets/main.js
+++ assets/main.js
@@ -7,10 +7,12 @@ function createImageGallery(parent, images){
     console.log("start");
     const root = document.createElement("div");
     root.style.position = "relative";
+    root.setAttribute("currentImage", "0");
 
     root.style.width = "100%";
     root.style.height = "auto";
-    root.style.aspectRatio = "16/9";
+    const aspect = images[0].width / images[1].height;
+    root.style.aspectRatio = aspect;
     root.style.overflow = "hidden";
     root.style.borderStyle = "solid";
     root.style.borderColor = "green";
@@ -18,10 +20,22 @@ function createImageGallery(parent, images){
     const btn_prev = document.createElement("button");
     btn_prev.textContent = "previous";
     btn_prev.style.position = "absolute";
+    btn_prev.style.left = "0px";
     btn_prev.style.top = "50%";
     btn_prev.style.zIndex = "100";
     btn_prev.style.transform = "translateY(-50%)";
     btn_prev.style.height = "100px";
+
+    btn_prev.onclick = ()=>{
+        let i = parseInt(root.getAttribute("currentImage"));
+        i--;
+        if(i < 0){
+            i = images.length-1;
+        }
+
+        setImage(i);
+    };
+
     const btn_next = document.createElement("button");
     btn_next.textContent = "next";
     btn_next.style.position = "absolute";
@@ -30,6 +44,25 @@ function createImageGallery(parent, images){
     btn_next.style.zIndex = "100";
     btn_next.style.transform = "translateY(-50%)";
     btn_next.style.height = "100px";
+
+    /**
+     * 
+     * @param {number} index 
+     */
+    const setImage = (index) => {
+        img_slider.style.transform = `translateX(${index * -100}%)`;
+        root.setAttribute("currentImage", index.toString());
+    };
+
+    btn_next.onclick = ()=>{
+        let i = parseInt(root.getAttribute("currentImage"));
+        i++;
+        if(i >= images.length){
+            i = 0;
+        }
+
+        setImage(i);
+    };
 
     const img_win = document.createElement("div");
     img_win.style.height = "100%";
